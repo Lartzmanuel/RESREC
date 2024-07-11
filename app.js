5588c19cb9859a6add3293b96e1c7d3f50ab3b5b
Lartzmanuel <teyemmanuel01@gmail.com>
2024-07-11 14:22:23 +0000

    Update files: app.js, package-lock.json, package.json, public/css/style.css, public/js/script.js, server/routes/main.js, views/about.ejs, views/home.ejs, views/layouts/main.ejs, views/login.ejs, views/partials/header.ejs, views/register.ejs. Add model/ and passport-config.js.

--- app.js
+++ app.js
@@ -1,22 +1,112 @@
-require('dotenv').config()
 
-//requiring installed packages
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const app = express();
-const PORT = 5000 || process.env.PORT;
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
-app.use(express.static('public'));
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+const expressLayouts = require('express-ejs-layouts')
+const bcrypt = require('bcrypt');
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const User = require('./model/users')
 
-//Templating engine
-app.use(expressLayout);
-app.set('view engine', 'ejs');
-app.set('layout', './layouts/main');
+
+mongoose.connect(process.env.DATABASE_URL)
+    .then(()=> {
+        console.log('Successfully connected to Database')
+    })
+    .catch(err => console.error('Could not connect to Database:', err))
 
 
-app.use('/', require('./server/routes/main'))
+const initialize = require('./passport-config')
+initialize(passport)
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+app.set('layout', './layouts/main')
+
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index', {name: req.user.name})
 })
 
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        await user.save();
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(await User.find()); //log all users in the database
+})
+
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+       return res.redirect('/home')
+    }
+    next()
+}
+
+app.delete('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/login');
+    });
+});
+
+
+// Importing and using routes from main.js
+const mainRoutes = require('./server/routes/main');
+app.use('/', mainRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
